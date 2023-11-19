fe71a036580a1abc0ff7c3105488f0dfa2c6af8f
Victor Redondo Mula <victor.redondo@opendeusto.es>
2023-11-19 23:38:37 +0100

    Revert "Merge branch 'main' of https://github.com/MariaBolanosD/Astrolone_"
    This reverts commit ee02cf6c8d28f2d2b9de5d3d5c833cdc01870e27, reversing
    changes made to a057cdda8821c5d144b92b86b65285717d12c287.

--- core/src/com/astrolone_/PantallaDeJuego.java
+++ core/src/com/astrolone_/PantallaDeJuego.java
@@ -2,139 +2,42 @@ package com.astrolone_;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.ScreenAdapter;import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-
-import Menus.PauseMenu;
-import Objetos.jugador.Jugador;
-import Objetos.jugador.Jugador.Direction;
 import ayudas.Constantes;
 
-public class PantallaDeJuego extends ScreenAdapter {
+public class PantallaDeJuego extends ScreenAdapter{
 	
-	//private OrthographicCamera camara;
+	private OrthographicCamera camara;
 	private SpriteBatch batch;
 	private World mundo;
 	private Box2DDebugRenderer box2DDebugRenderer;
 	
-	private AstroLone_Juego game;
-	private static final String SCREEN_NAME = "Game Screen";
-	private Stage stage;
 	
-	private Jugador jugador;
-	
-//	public PantallaDeJuego(OrthographicCamera camara) {
-//		this.camara = camara;
-//		this.batch = new SpriteBatch();
-//		this.mundo = new World(new Vector2(0, 0), false);
-//		this.box2DDebugRenderer = new Box2DDebugRenderer();
-//	}
-
-	class KeyboardProcessor extends InputAdapter {
-
-		
-		
-		@Override
-		public boolean keyDown(int keycode) {
-			
-			switch (keycode) {
-			case Keys.LEFT:jugador.setLeftMove(true);
-				break;
-			case Keys.RIGHT:jugador.setRightMove(true);
-				break;
-			case Keys.UP:jugador.setUpMove(true);
-				break;
-			case Keys.DOWN:jugador.setDownMove(true);
-				break;
-			default:
-				break;
-			}
-			
-			
-			return super.keyDown(keycode);
-		}
-		
-		@Override
-		public boolean keyUp(int key) {			
-			switch (key) {
-			
-				case Keys.LEFT:		jugador.setLeftMove(false);
-									break;
-				case Keys.RIGHT:	jugador.setRightMove(false);
-									break;
-				case Keys.UP:		jugador.setUpMove(false);
-									break;
-				case Keys.DOWN:		jugador.setDownMove(false);
-									break;
-				case Keys.ESCAPE:	toPauseScreen();
-									break;
-				case Keys.F:		toogleFullScreen();
-									break;
-				default:			break;
-			}
-
-			return false;
-		}
-		
-		private void toogleFullScreen() {
-			if (game.isFullScreen()) {
-				game.setWindowed();
-			} else {
-				game.setFullscreen();
-			}
-		}
-		
-		public void toPauseScreen() {
-			game.getScreen().dispose();
-			game.setScreen(new PauseMenu(game));
-		}
-	}
-	
-	public PantallaDeJuego(AstroLone_Juego game) {		
-		this.game = game;
-		
+	public PantallaDeJuego(OrthographicCamera camara) {
+		this.camara = camara;
 		this.batch = new SpriteBatch();
 		this.mundo = new World(new Vector2(0, 0), false);
 		this.box2DDebugRenderer = new Box2DDebugRenderer();
-
-		BodyDef bDef = new BodyDef(); bDef.type = BodyDef.BodyType.DynamicBody;
-		this.jugador = new Jugador(20, 20, mundo.createBody(bDef));
-		
-		Gdx.app.log(SCREEN_NAME, "Iniciando screen principal del juego");
-		
-		stage = new Stage(game.getViewport());
-		
-		InputMultiplexer multiplexer = new InputMultiplexer();
-		multiplexer.addProcessor(stage);
-		multiplexer.addProcessor(new KeyboardProcessor());
-		
-		// registramos el multiplexador de eventos como escuchador
-		Gdx.input.setInputProcessor(multiplexer);
 	}
 
 	private void update() {
 		mundo.step(1/60, 6, 2);
-		jugador.update();
-		batch.setProjectionMatrix(game.getCamera().combined);
+		
+		batch.setProjectionMatrix(camara.combined);
 		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
 			Gdx.app.exit();
 		}
 	}
 	
 	private void updateCamara() {
-		game.getCamera().position.set(new Vector3(0,0,0));
-		game.getCamera().update();
+		camara.position.set(new Vector3(0,0,0));
+		camara.update();
 	}
 	
 	
@@ -147,12 +50,12 @@ public class PantallaDeJuego extends ScreenAdapter {
 		
 		batch.begin();
 		//Render de objetos
-		//box2DDebugRenderer.render(mundo, game.getCamera().combined.scl(Constantes.pixelesPorMetro));
 		
-		jugador.render(batch);
+		
 		
 		batch.end();
 		
+		box2DDebugRenderer.render(mundo, camara.combined.scl(Constantes.pixelesPorMetro));
 		
 	}
 	
